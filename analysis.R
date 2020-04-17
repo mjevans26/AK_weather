be85d01c2e23699ffff3e7e1de4e7bd60bd66e01
mjevans26 <evans.michael26@gmail.com>
2020-04-17 13:11:46 -0400

    Create small analysis vignette

--- analysis.R
+++ analysis.R
@@ -1,17 +1,43 @@
 library(dplyr)
 library(plotly)
 
-filter(cleandata, Month == 1|Month == 12)%>%
+cleandata <- mutate(cleandata, survey = FLIR & !daylight)
+
+axis <- list(
+  tickfont = list(color = 'black', size = 14),
+  titlefont = list(color ='black', size = 16),
+  showgrid = FALSE
+)
+
+y2 <- append(
+  list(
+    overlaying = "y",
+    side = "right",
+    title = "Observed hours",
+    zeroline = FALSE,
+    range = c(0, 5500)),
+  axis
+)
+
+filter(cleandata, Month == 1|Month == 12, !is.na(survey))%>%
   group_by(Year)%>%
-  summarize(prop = sum(FLIR, na.rm = TRUE)/n())%>%
-  plot_ly(type = 'bar', x = ~Year, y = ~prop)%>%
+  summarize(prop = sum(survey, na.rm = TRUE)/n(),
+            count = n())%>%
+  plot_ly()%>%
+  add_trace(name = 'Suitable (%)',
+            type = 'bar', x = ~Year, y = ~prop,
+            text = ~paste('Suitable hours (%) in', Year, '=', round(prop, 2)),
+            hoverinfo = 'text')%>%
+  add_trace(name = 'Hours',
+            type = 'scatter', mode = 'lines',
+            x = ~Year, y = ~count,
+            yaxis = 'y2')%>%
   layout(
-    xaxis = list(
-      title = 'Year'
-    ),
-    yaxis = list(
-      title = 'Suitable hours (%)'
-    )
+    legend = list(x = 0.8, y = 1, yanchor = 'left'),
+    xaxis = append(list(title = 'Year'), axis),
+    yaxis = append(list(title = 'Suitable hours (%)'), axis),
+    yaxis2 = y2,
+    margin = list(t = 0, b = 0, l = 55, r = 55)
   )
 
-logModel <- glm(data = filter(cleandata, Month ==1|Month ==12), FLIR ~ Year, family = binomial(link = 'logit'))
+logModel <- glm(data = filter(cleandata, Month ==1|Month ==12), survey ~ Year, family = binomial(link = 'logit'))
--- cleaning.R
+++ cleaning.R
@@ -1,7 +1,14 @@
 library(dplyr)
 library(lubridate)
 
-#' Identify suitable flying conditions for FLIR using hourly weather data from
+# https://www.ncdc.noaa.gov/cdo-web/datasets/LCD/stations/WBAN:27401/detail
+
+#' Identify suitable flying conditions for FLIR using hourly weather data
+#' 
+#' @description Wilson & Durner (2019) describe optimal FLIR conditions as
+#' "surface wind speeds <11 km/hr;
+#'  dew point‐ambient temp spread of >3.0°C,
+#'  and no visible mois- ture such as fog or precipitation"
 #' 
 #' @param wind dataframe field storing wind speed
 #' @param dewT dataframe field storing dewpoint temperature
@@ -11,10 +18,12 @@ library(lubridate)
 test_flir <- function(wind, dewT, bulbT, type){
   windspeed <- as.numeric(wind) < 11
   temp <- (as.numeric(bulbT) - as.numeric(dewT)) > 3
-  precip <- !grepl("SN|RA|FG", type)
+  precip <- !grepl("SN|RA|FG|PL|GR|HZ|DZ|BR", type)
   goodConditions <- windspeed & temp & precip
   return(goodConditions)
 }
+
+
 
 file1 <- read.csv(file='data-raw/Hourly_01Jan11_08Mar20.csv', header = TRUE, na.strings = "", stringsAsFactors = FALSE)%>%
   mutate(DATE = ymd_hms(DATE),
@@ -34,8 +43,40 @@ file3 <- read.csv(file='data-raw/Hourly_01Jan97_31Dec99.csv', header = TRUE, na.
 dat <- bind_rows(select(file1, DATE, FLIR), select(file2, DATE, FLIR), select(file3, DATE, FLIR))
 
 cleandata <- dat%>%
-  mutate(Month = month(DATE),
+  mutate(Day = day(DATE),
+         Month = month(DATE),
          Year = year(DATE),
          Hour = hour(DATE))
 
+# Define Date-Hour combos during which there is daylight, which is unsuitable
+# https://www.timeanddate.com/sun/usa/kaktovik
 
+# daylight hours during january and december at kaktovik AK
+daylight <- list('jan31'= seq(10, 14),
+                 'jan25'= seq(11, 14),
+                 'jan22'= seq(11, 13),
+                 'jan18'= seq(12,13)
+)
+
+
+daylight <- function(Month, Day, Hour){
+  if(Month == 1 & Day >= 18 & Day < 22 & Hour %in% daylight[['jan18']]){
+    'yes'
+  }else if(Month == 1 & Day >= 22 & Day <25 & Hour %in% daylight[['jan22']]){
+    'yes'
+  }else if(Month == 1 & Day >= 25 & Day <31 & Hour %in% daylight[['jan25']]){
+    'yes'
+  }else if(Month == 1 & Day == 31 & Hour %in% daylight[['jan31']]){
+    'yes'
+  }else{
+    'no'
+  }
+}
+
+cleandata$daylight <- FALSE
+cleandata$daylight[cleandata$Month == 1 & cleandata$Day >= 18 & cleandata$Day < 22 & cleandata$Hour %in% seq(12,13)] <- TRUE
+cleandata$daylight[cleandata$Month == 1 & cleandata$Day >= 22 & cleandata$Day <25 & cleandata$Hour %in% seq(11,13)] <- TRUE
+cleandata$daylight[cleandata$Month == 1 & cleandata$Day >= 25 & cleandata$Day <31 & cleandata$Hour %in% seq(11,14)] <- TRUE
+cleandata$daylight[cleandata$Month == 1 & cleandata$Day == 31 & cleandata$Hour %in% seq(10,14)] <- TRUE
+
+saveRDS(cleandata, file = 'data/cleandata.rds')
